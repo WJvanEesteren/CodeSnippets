a5df4a325aa1cdd15a3d62fd418fefc3fac8e431
jamievanee <jvaneesteren@deloitte.nl>
2020-11-10 14:28:57 +0100

    added ability to refer to file in higher path

--- relative_path.py
+++ relative_path.py
@@ -1,3 +1,10 @@
-from os.path import join, dirname, realpath
+from os.path import join, dirname, realpath, abspath
 
+#refer to file in current directory
 results_path = join(dirname(realpath(__file__)), 'results')
+
+#refer to file in one directory higher
+path = abspath(join(realpath(dirname(__file__)),'..', 'test.py'))
+
+print(results_path)
+print(path)
